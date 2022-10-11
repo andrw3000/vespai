ee1048db62564e563b4c9ccddc8982db4fe975bc
Andrew Corbett <90460047+andrw3000@users.noreply.github.com>
2022-10-11 11:36:46 +0100

    Updated figures

--- models/yolov5csvplot.py
+++ models/yolov5csvplot.py
@@ -4,11 +4,12 @@ import glob
 import argparse
 import pandas as pd
 
-# Root in `vespalert`
-if not os.path.basename(os.getcwd()) == 'vespalert':
-    assert "Please run `csvplot.py` from root directory `vespalert`."
+# Root in `vespai`
+vespai_dir = os.getcwd()
+if not os.path.basename(vespai_dir) == 'vespai':
+    assert "Please run `csvplot.py` from root directory `vespai`."
 
-sys.path.insert(0, os.path.join(os.getcwd(), '../models/yolov5'))
+sys.path.insert(0, os.path.join(vespai_dir, 'models/yolov5'))
 from utils.plots import plot_results
 
 parser = argparse.ArgumentParser()
@@ -26,11 +27,11 @@ args = parser.parse_args()
 if __name__ == "__main__":
 
     # Plot entire results file
-    csv_dir = os.path.join('../models/yolov5/runs/train', args.dir)
+    csv_dir = os.path.join(vespai_dir, 'models/yolov5-runs/train', args.dir)
     plot_results(file='', dir=csv_dir)
 
     # Plot fist nrows rows
-    csv_files = glob.glob(os.path.join(csv_dir, 'results*.csv'))
+    csv_files = glob.glob(os.path.join(csv_dir, '*.csv'))
     new_dir = os.path.join(csv_dir, 'first{}eps'.format(args.nrows))
     os.makedirs(new_dir, exist_ok=True)
 
